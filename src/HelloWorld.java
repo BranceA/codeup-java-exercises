85364040626d50a38af96150d6de49a10cf81c94
BranceA <brancealexander@gmail.com>
2019-09-16 16:46:34 -0500

    added second exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,26 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.print("Hello World!");
-        System.out.print("Row Row Fight the Power!");
+        System.out.println("Hello World!");
+        System.out.println("Row Row Fight the Power!");
+        int MyFavoriteNumber = 664;
+        System.out.println(MyFavoriteNumber);
+        String myString = "Yo";
+        System.out.println(myString);
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+        int w = 5;
+        System.out.println(++w);
+        System.out.println(w);
+        int x = 4 + 5;
+        int h = 3;
+        int i = 4 * h;
+        int e = 10;
+        int f = 2;
+        e /= f;
+        f -= e;
+        System.out.println(e);
+        System.out.println(f);
     }
 
 }
