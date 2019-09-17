2b8ba42e6d3d024ea9134bb293c4e49896cac287
BranceA <brancealexander@gmail.com>
2019-09-17 15:46:01 -0500

    done with control flow time for bonus

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -5,15 +5,26 @@ public class ControlFlowExercises {
     public static void main (String[] args){
 
         Scanner input = new Scanner(System.in);
-        System.out.println("ENTER A NUMBER!!!");
         boolean keepGoing = true;
-        int inputNumber = input.nextInt();
-        int i = 1;
-        while (keepGoing && i <= inputNumber) {
-            System.out.println(i + ": Squared is: " + (i * i) + " Cubed is: " + (i * i * i));
-            i++;
-            System.out.println("Continue?");
+
+        while (keepGoing){
+            System.out.println("ENTER YOUR GRADE!!!");
+            byte gradeInput = input.nextByte();
+            if (gradeInput >= 90 && gradeInput <= 100) {
+                System.out.println("You got an A");
+            } else if (gradeInput >= 80 && gradeInput <= 89) {
+                System.out.println("You got a B");
+            } else if (gradeInput >= 70 && gradeInput <= 79) {
+                System.out.println("You got a C");
+            } else if (gradeInput >= 60 && gradeInput <= 69) {
+                System.out.println("You got a D");
+            } else if (gradeInput < 60) {
+                System.out.println("You fail my dude");
+            }
+
+            System.out.println("Keep going?");
             keepGoing = input.nextBoolean();
+
         }
 
     }
