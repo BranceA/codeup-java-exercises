3406161f8c4dc35c878a1231c5d0f641435f9f00
BranceA <brancealexander@gmail.com>
2019-09-17 14:55:55 -0500

    done with control flow two

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,10 +2,10 @@ public class ControlFlowExercises {
 
     public static void main (String[] args){
 
-        int i = 5;
-        while (i <= 15){
+        for(long i = 2; i < 1000000; i *= i) {
+
             System.out.println(i);
-            i++;
+
         }
 
     }
