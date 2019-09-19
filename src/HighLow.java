62685e1fa2d63dd107e71945bf04fc0bffe93084
BranceA <brancealexander@gmail.com>
2019-09-19 16:53:11 -0500

    did highlow bonuses

--- src/HighLow.java
+++ src/HighLow.java
@@ -4,22 +4,28 @@ public class HighLow {
 
     public static void main(String[] args) {
 
-        System.out.println("I'm thinking of a number between 1 and 100. Guess what it is.");
+        System.out.println("I'm thinking of a number between 1 and 100. Guess what it is. You have 10 guesses.");
         int rand = (int)(Math.random() * 99) + 1;
-        playTheGame(rand);
+        playTheGame(rand, 0);
 
     }
 
-    public static void playTheGame(int rand){
+    public static void playTheGame(int rand, int guesses){
         int playerNumber = MethodsExercises.getInteger(1, 100);
-        if(playerNumber > rand){
+        guesses += 1;
+        if(guesses == 10){
+            System.out.println("YOU LOSE!!");
+        }else if(playerNumber > rand){
             System.out.println("LOWER!");
-            playTheGame(rand);
+            System.out.println("You have guessed " + guesses + " times");
+            playTheGame(rand, guesses);
         }else if (playerNumber < rand){
             System.out.println("HIGHER!");
-            playTheGame(rand);
+            System.out.println("You have guessed " + guesses + " times");
+            playTheGame(rand, guesses);
         }else if (playerNumber == rand){
             System.out.println("GOOD GUESS!");
+            System.out.println("You have guessed " + guesses + " times");
         }
     }
 
